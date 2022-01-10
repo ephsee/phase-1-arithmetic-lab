607ac56427bf9ef5d37af8f1bcd01aec1f5e9e1c
Francois Spies <fcspies@gmail.com>
2022-01-09 23:03:15 -0500

    commit tree b86cbef3187a51db85c912611daf8832c9f1c281

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 7
+let num2 = 9
+const multiply = num1 * num2 - 1
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 64
+let num4 = 20
+const mod = (num3 % num4)
+
+const max = Math.max(1, 14, 2, 16, 7, 3, 20, 9, 5)
